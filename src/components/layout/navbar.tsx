06d440ecb304e5c8e2f4dcf53344ab88ea61fbe0
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2025-01-02 00:56:22 +0200

    update navbar large screen

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -41,8 +41,10 @@ export function Navbar() {
   ];
 
   return (
-    <nav className="container bg-white rounded-navbar flex justify-between items-center p-5 lg:px-10 md:px-7 px-5 sticky top- z-50">
-      <LogoIcon className="md:!w-36 w-28 lg:min-h-10 min-h-full" />
+    <nav className="container bg-white rounded-navbar flex justify-between items-center p-5 lg:px-10 md:px-7 px-5 sticky sm:top-12 top-6 z-50">
+      <a href="#home">
+        <LogoIcon className="md:!w-36 w-28 md:h-6 h-6 min-h-full" />
+      </a>
 
       <ul className="flex gap-4">
         {navItems.map((item) => (
